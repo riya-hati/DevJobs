3547d22f48a6c622b10072d2572bb776c6b42927
riya-hati <riyahati01@gmail.com git config --global user.name riya-hatigit config --global user.email riyahati01@gmail.com>
2024-02-27 11:14:28 +0530

    initial commit

--- Devjobs/src/App.jsx
+++ Devjobs/src/App.jsx
@@ -1,17 +1,22 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import Joblist from './components/Jobs/Joblist';
-import JobDetails from './components/Job-details/JobDetails';
+import "./App.css";
+import Header from "./components/Header";
+import Joblist from "./components/Jobs/Joblist";
+import JobDetails from "./components/Job-details/JobDetails";
 // import {Routes, Route, Navigate } from 'react-router-dom'
 // import './assets/react.svg' from '/src/App.jsx'
 function App() {
   return(
     <div>
       <Header/>
-       <Routes></Routes>
+       <Routes>
+        <Route path='/' element={<Navigate to='/Jobs'/>} />
+        <Route path='/jobs' element={<Joblist/>} />
+        <Route path='/jobs/:position' element={<JobDetails/>} />
+
+       </Routes>
     </div>
   );
 }
